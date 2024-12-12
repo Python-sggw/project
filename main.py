14b3527ba8904551072a12959c2645d24330951b
JasiekJasiek <janszala.04@gmail.com>
2024-12-12 18:07:53 +0100

    added photos to db

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask
+import os
+from flask import Flask, request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+
+db = SQLAlchemy(app)
+
+class Photos(db.Model):
+    photo_id = db.Column(db.Integer, primary_key=True)
+    likes = db.Column(db.Integer, default=0)
+    comments = db.Column(db.String(4096), default = '')
+    # user_id = db.Column(db.Integer, backref=db.backref('Users'))
+    date = db.Column(db.DateTime, default=datetime.now)
 
 @app.route('/')
-def main_page():
+def login():
     return "na dzis koniec"
+
+
+@app.route('/main_page')
+def main_page():
+    pass
 
 if __name__ == '__main__':
     app.run()
