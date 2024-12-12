b4ef0d8ee59971a06fbdb6fa5fb01f6d03054952
JasiekJasiek <janszala.04@gmail.com>
2024-12-12 19:28:02 +0100

    added static, templates, instance and main_page in flaks

--- main.py
+++ main.py
@@ -9,20 +9,64 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
 class Photos(db.Model):
-    photo_id = db.Column(db.Integer, primary_key=True)
+    photo_id = db.Column(db.String(255), primary_key=True)
     likes = db.Column(db.Integer, default=0)
     comments = db.Column(db.String(4096), default = '')
     # user_id = db.Column(db.Integer, backref=db.backref('Users'))
     date = db.Column(db.DateTime, default=datetime.now)
 
+def add():
+    photo1 = Photos( 
+        photo_id = "1.jpg",
+        likes = 10,
+        comments = "Fajny Samolot\nFajny\nNiezly!"
+    )
+    db.session.add(photo1)
+    db.session.commit()
+    photo1 = Photos( 
+        photo_id = "2.jpg",
+        likes = 1,
+        comments = "Fajny Samolot!\nFajny!\nNiezly!"
+    )
+    db.session.add(photo1)
+    db.session.commit()
+    photo1 = Photos( 
+        photo_id = "3.jpg",
+        likes = 5,
+        comments = "Fajny Samolot!!!\nFajny\nNiezly!"
+    )
+    db.session.add(photo1)
+    db.session.commit()
+    photo1 = Photos( 
+        photo_id = "4.jpg",
+        likes = 20,
+        comments = "Fajny Samolot\nFajny!!!!\nNiezly!"
+    )
+    db.session.add(photo1)
+    db.session.commit()
+    photo1 = Photos( 
+        photo_id = "5.jpg",
+        likes = 30,
+        comments = "Fajny\nFajny\nNiezly!"
+    )
+    db.session.add(photo1)
+    db.session.commit()
+
 @app.route('/')
-def login():
-    return "na dzis koniec"
-
-
-@app.route('/main_page')
 def main_page():
-    pass
+    #add()
+    photos = Photos.query.all()
+    posts = [
+        {
+            "username": "Unknown",
+            "image": f"./photos/{photo.photo_id}",
+            "likes": photo.likes,
+            "comments": photo.comments.split('\n')
+        } for photo in photos
+    ]
+    return render_template('main_page.html', posts=posts)
 
 if __name__ == '__main__':
+    # with app.app_context():
+    #     db.create_all()
     app.run()
